58a791132f1b58b8759b9f7d52ac7857b2412955
Kye <130492321+kyedevelopment@users.noreply.github.com>
2024-09-13 08:04:32 -0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  color: "FF0000", // color for embeds
+
+  owner_ids: [], // put your server id here
+
+  timezone: "America/Los_Angeles",
+
+  presence: {
+    status: "dnd",
+    activities: [
+      {
+        name: "For Commands", // text of your bot status
+        type: 3, // 1 - 5
+      },
+    ],
+  },
+};
